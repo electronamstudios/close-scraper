cf08ca3708802db930551e9dc7a28753fd892c6a
El3ctroNam Studios <83252177+svensk007@users.noreply.github.com>
2023-12-11 08:51:20 -0600

    mapping out idea still but have solid ground

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+# https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=1686491104&period2=1702305859&interval=1d&events=history&includeAdjustedClose=true
+#                                                       ^
+#                                                       |
+#                                       change that for the wanted ticker
+
 import requests, datetime
 from bs4 import BeautifulSoup
