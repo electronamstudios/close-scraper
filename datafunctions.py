5c9caf095bbbaba25f3e69f3c5065378212df083
El3ctroNam Studios <83252177+svensk007@users.noreply.github.com>
2023-12-12 20:15:41 +0000

    made a separate file for all the functions to increase code clarity

---
+++ datafunctions.py
@@ -0,0 +1,58 @@
+import os
+import shutil
+import pandas as pd
+from urllib.request import urlretrieve
+from colorama import Fore, Style
+
+class dataFunctions:
+    def fetchData(ticker, timeStart, timeEnd):
+        try:
+            URL = "https://query1.finance.yahoo.com/v7/finance/download/" + ticker + "?period1=" + str(timeStart) + "&period2=" + str(timeEnd) + "&interval=1d&events=history&includeAdjustedClose=true"
+            print(Fore.BLUE + "Fetching " + Style.RESET_ALL + ticker + " from " + URL + "\n")
+
+            if not os.path.exists('./temp'):
+                os.makedirs('./temp')
+
+            urlretrieve(URL, "./temp/" + ticker + ".csv")
+            print(Fore.GREEN + "Success" + Style.RESET_ALL + " Fetching " + ticker + "\n" )
+        except:
+            print(Fore.RED + "Error" + Style.RESET_ALL + " Fetching Ticker \'" + ticker + "\'\n")
+
+    def mergeData(csv_files):
+        dataframeList = []
+
+        for filename in csv_files:
+            # Read the csv file
+            dataframe = pd.read_csv(filename)
+
+            # Extract csv file name and add it as a new column
+            ticker_name = os.path.splitext(os.path.basename(filename))[0]
+
+            # Insert the 'Ticker' column at the second position of the dataframe
+            dataframe.insert(1, 'Ticker', ticker_name)
+
+            # Append the dataframe to the list
+            dataframeList.append(dataframe)
+
+        # Concatenate all dataframes in the list
+        combinedDataframe = pd.concat(dataframeList)
+
+        return combinedDataframe
+
+    def pivotData(combinedDataframe, closeType):
+        if closeType == "Adj":
+            closeType = "Adj Close"
+        elif closeType == "Close":
+            closeType = "Close"
+
+        try:
+            # Pivot the DataFrame so 'Ticker' becomes the columns and closeType becomes the row corresponding to the ticker
+            pivotDataframe = combinedDataframe.pivot(index='Date', columns='Ticker', values=closeType)
+        except:
+            print(Fore.RED + "Error" + Style.RESET_ALL + " with closeType \'" + closeType + "\'")
+            shutil.rmtree('./temp')
+            exit()
+
+        # Write the pivoted dataframe to a new csv file in /out directory
+        pivotDataframe.to_csv('./out.csv')
+
--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import glob
 import time
 import shutil
 import datetime
-import pandas as pd
+import datafunctions as ff
 from colorama import Fore, Style
-from urllib.request import urlretrieve
 
 # ------------------------------------ Setup
 
@@ -20,75 +19,21 @@ for i in range(tickerCount):
     tickerList.append(ticker)
 
 print("\nGenerating URLs for " + str(tickerList) + "\n")
-
-# ------------------------------------ Fetch Data
-
-def fetchData(ticker, timeStart, timeEnd):
-    try:
-        URL = "https://query1.finance.yahoo.com/v7/finance/download/" + ticker + "?period1=" + str(timeStart) + "&period2=" + str(timeEnd) + "&interval=1d&events=history&includeAdjustedClose=true"
-        print(Fore.BLUE + "Fetching " + Style.RESET_ALL + ticker + " from " + URL + "\n")
-
-        if not os.path.exists('./temp'):
-            os.makedirs('./temp')
-
-        urlretrieve(URL, "./temp/" + ticker + ".csv")
-        print(Fore.GREEN + "Success" + Style.RESET_ALL + " Fetching " + ticker + "\n" )
-    except:
-        print(Fore.RED + "Error" + Style.RESET_ALL + " Fetching Ticker \'" + ticker + "\'\n")
-
-def mergeData(csv_files):
-    dataframeList = []
-
-    for filename in csv_files:
-        # Read the csv file
-        dataframe = pd.read_csv(filename)
-
-        # Extract csv file name and add it as a new column
-        ticker_name = os.path.splitext(os.path.basename(filename))[0]
-        
-        # Insert the 'Ticker' column at the second position of the dataframe
-        dataframe.insert(1, 'Ticker', ticker_name)
-
-        # Append the dataframe to the list
-        dataframeList.append(dataframe)
-
-    # Concatenate all dataframes in the list
-    combinedDataframe = pd.concat(dataframeList)
-
-    return combinedDataframe
-
-def pivotData(combinedDataframe, closeType):
-    if closeType == "Adj":
-        closeType = "Adj Close"
-    elif closeType == "Close":
-        closeType = "Close"
-    
-    try:
-        # Pivot the DataFrame so 'Ticker' becomes the columns and closeType becomes the row corresponding to the ticker
-        pivotDataframe = combinedDataframe.pivot(index='Date', columns='Ticker', values=closeType)
-    except:
-        print(Fore.RED + "Error" + Style.RESET_ALL + " with closeType \'" + closeType + "\'")
-        shutil.rmtree('./temp')
-        exit()
-
-    # Write the pivoted dataframe to a new csv file in /out directory
-    pivotDataframe.to_csv('./out.csv')
-
 for i in range(tickerCount):
-    fetchData(tickerList[i], timeStart, timeEnd)
+    ff.fetchData(tickerList[i], timeStart, timeEnd)
 
 # ------------------------------------ Merge Data
 
 # Get the csv files in the /temp directory
 csv_files = glob.glob('./temp/*.csv')
 
-combinedDataframe = mergeData(csv_files)
+combinedDataframe = ff.mergeData(csv_files)
 
 closeType = input("Enter 'Adj' or 'Close' to choose the type of value you want to see: ")
 
 print("\n" + Fore.BLUE + "Merging" + Style.RESET_ALL + " .csv files...")
 
-pivotData(combinedDataframe, closeType)
+ff.pivotData(combinedDataframe, closeType)
 
 if os.path.exists('./temp'):
     shutil.rmtree('./temp')
