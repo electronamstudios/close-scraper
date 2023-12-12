1d39a41e9ef58d32bc3956605f83ea1b5e15612c
svensk007 <83252177+svensk007@users.noreply.github.com>
2023-12-11 20:44:08 -0600

    refactoring code i didnt even commit

--- main.py
+++ main.py
@@ -23,53 +23,69 @@ print("\nGenerating URLs for " + str(tickerList) + "\n")
 
 # ------------------------------------ Fetch Data
 
-for i in range(len(tickerList)):
+def fetchData(ticker, timeStart, timeEnd):
     try:
-        URL = "https://query1.finance.yahoo.com/v7/finance/download/" + tickerList[i] + "?period1=" + str(timeStart) + "&period2=" + str(timeEnd) + "&interval=1d&events=history&includeAdjustedClose=true"
-        print(Fore.BLUE + "Fetching " + Style.RESET_ALL + tickerList[i] + " from " + URL + "\n")
+        URL = "https://query1.finance.yahoo.com/v7/finance/download/" + ticker + "?period1=" + str(timeStart) + "&period2=" + str(timeEnd) + "&interval=1d&events=history&includeAdjustedClose=true"
+        print(Fore.BLUE + "Fetching " + Style.RESET_ALL + ticker + " from " + URL + "\n")
 
         if not os.path.exists('./temp'):
             os.makedirs('./temp')
 
-        urlretrieve(URL, "./temp/" + tickerList[i] + ".csv")
-        print(Fore.GREEN + "Success" + Style.RESET_ALL + " Fetching " + tickerList[i] + "\n" )
+        urlretrieve(URL, "./temp/" + ticker + ".csv")
+        print(Fore.GREEN + "Success" + Style.RESET_ALL + " Fetching " + ticker + "\n" )
     except:
-        print(Fore.RED + "Error" + Style.RESET_ALL + " Fetching Ticker \'" + tickerList[i] + "\'\n")
+        print(Fore.RED + "Error" + Style.RESET_ALL + " Fetching Ticker \'" + ticker + "\'\n")
 
-print(Fore.BLUE + "Merging" + Style.RESET_ALL + " .csv files...")
+def mergeData(csv_files):
+    dataframeList = []
+
+    for filename in csv_files:
+        # Read the csv file
+        dataframe = pd.read_csv(filename)
+
+        # Extract csv file name and add it as a new column
+        ticker_name = os.path.splitext(os.path.basename(filename))[0]
+        
+        # Insert the 'Ticker' column at the second position of the dataframe
+        dataframe.insert(1, 'Ticker', ticker_name)
+
+        # Append the dataframe to the list
+        dataframeList.append(dataframe)
+
+    # Concatenate all dataframes in the list
+    combinedDataframe = pd.concat(dataframeList)
+
+    return combinedDataframe
+
+def pivotData(combinedDataframe, valueType):
+    if valueType == "Adj":
+        valueType = "Adj Close"
+    elif valueType == "Close":
+        valueType = "Close"
+
+    # Pivot the DataFrame so 'Ticker' becomes the columns and valueType becomes the row corresponding to the ticker
+    pivotDataframe = combinedDataframe.pivot(index='Date', columns='Ticker', values=valueType)
+
+    # Write the pivoted dataframe to a new csv file in /out directory
+    pivotDataframe.to_csv('./out.csv')
+
+for i in range(tickerCount):
+    fetchData(tickerList[i], timeStart, timeEnd)
 
 # ------------------------------------ Merge Data
 
 # Get the csv files in the /temp directory
 csv_files = glob.glob('./temp/*.csv')
 
-# List to store dataframes
-df_list = []
+combinedDataframe = mergeData(csv_files)
 
-# Loop through files
-for filename in csv_files:
-    # Read the csv file
-    df = pd.read_csv(filename)
+valueType = input("Enter 'Adj' or 'Close' to choose the type of value you want to see: ")
 
-    # Select only the "Date", "Adj Close", and "Close" columns
-    df = df[["Date", "Adj Close", "Close"]]
+print("\n" + Fore.BLUE + "Merging" + Style.RESET_ALL + " .csv files...")
 
-    # Extract csv file name and add it as a new column
-    ticker_name = os.path.splitext(os.path.basename(filename))[0]
-    
-    # Insert the 'Ticker' column at the second position of the dataframe
-    df.insert(1, 'Ticker', ticker_name)
-
-    # Append the dataframe to the list
-    df_list.append(df)
-
-# Concatenate all dataframes in the list
-combined_df = pd.concat(df_list)
-
-# Write the concatenated dataframe to a new csv file in /out directory
-combined_df.to_csv('./out.csv', index=False)
+pivotData(combinedDataframe, valueType)
 
 if os.path.exists('./temp'):
     shutil.rmtree('./temp')
 
-print(Fore.GREEN + "Finished!" + Style.RESET_ALL + " Output saved to ./out.csv")
+print("\n" + Fore.GREEN + "Finished!" + Style.RESET_ALL + " Output saved to ./out.csv \n")
